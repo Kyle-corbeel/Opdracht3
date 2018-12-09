3fc859f62f11583398d809b3e8e37aec5c0c4894
Kyle-corbeel <37397551+Kyle-corbeel@users.noreply.github.com>
2018-12-09 18:30:33 +0100

    Failure 1.0

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -5,5 +5,6 @@ import java.util.HashMap;
 public interface Login extends Remote {
     //HashMap<Integer, String> register(String ip) throws RemoteException;
     String getOwner(String fileName) throws RemoteException;
+    String getNeighbours(int nodeHash) throws RemoteException;
     //Boolean  remove(String ip) throws RemoteException;
 }
