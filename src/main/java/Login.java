7df78094a499c00b4c3490417e1dc2760d5fa4a0
WouterJac <37212861+WouterJac@users.noreply.github.com>
2018-12-20 09:15:59 +0100

    Delete Login.java

--- src/main/java/Login.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.HashMap;
-
-public interface Login extends Remote {
-    //HashMap<Integer, String> register(String ip) throws RemoteException;
-    String getOwner(String fileName) throws RemoteException;
-    //Boolean  remove(String ip) throws RemoteException;
-}
